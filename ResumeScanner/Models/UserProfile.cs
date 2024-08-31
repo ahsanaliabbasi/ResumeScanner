71bca50d4a07911efcdfe0e7fdd65a1cf1d95865
Ahsan Ali Abbasi <Ahsan.AliAbbasi@Teradata.com>
2024-08-31 23:55:41 +0500

    ModelsAdded

---
+++ ResumeScanner/Models/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace ResumeScanner.Models
+{
+    public class UserProfile
+    {
+        public string jobTitle { get; set; }
+        public int yearsOfExperience { get; set; }
+        public string nameOfCurrentCompany { get; set; }
+        public int graduationYear { get; set; }
+        public string universityName { get; set;}
+        public int mastersDegreeStatus { get; set; }
+        public int phdDegreeStatus { get; set; }
+    }
+}
