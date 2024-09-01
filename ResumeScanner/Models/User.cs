35b60ce8a129250c6153cddda092434c5db28f75
Ahsan Ali Abbasi <Ahsan.AliAbbasi@Teradata.com>
2024-09-01 13:42:41 +0500

    ValidationsAddedIntoModels

--- ResumeScanner/Models/User.cs
+++ ResumeScanner/Models/User.cs
@@ -1,10 +1,18 @@
-﻿namespace ResumeScanner.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResumeScanner.Models
 {
     public class User
     {
         public int userId { get; set; }
+
+        [Required]
         public string firstName { get; set; }
+        [Required]
         public string lastName { get; set; }
+
+        [EmailAddress(ErrorMessage ="Invalid Email Address!")]
+        [Required]
         public string email { get; set; }
 
 
